8f403aaff91af5efdc4741236ad9b23a384a59d2
AkhmaevM <m.axmaev@yandex.ru>
2026-02-07 17:45:06 +0300

    Theme handler function was updated

--- js/index.js
+++ js/index.js
@@ -7,43 +7,65 @@ const btnClear = document.querySelector('.btn-clear');
 const appWrapper = document.querySelector('.calculator__main');
 const deleteBtn = document.querySelector('.btn-delete');
 const changeSignBtn = document.querySelector('.btn-changeSign');
-const btnTop = document.querySelector('.btn-top');
-const btnRight = document.querySelector('.btn-right');
+const btnTop = document.querySelectorAll('.btn-top');
+const btnRight = document.querySelectorAll('.btn-right');
 const calculator = document.querySelector('.calculator');
 const deleteIcon = document.querySelector('.delete-icon');
 const lightThemeBtn = document.querySelector('.calculator__theme-light');
 const darkThemeBtn = document.querySelector('.calculator__theme-dark');
+const body = document.querySelector('.body');
 
 
-console.log(changeSignBtn);
 
 
+// for (let i = 0; i < btnRight.length; i++) {
+//     btnRight[i].style.backgroundColor = '#4B5EFC';
+//     btnRight[i].style.color = 'white';
+// }
 
-for (let i = 0; i < btnRight.length; i++) {
-    btnRight[i].style.backgroundColor = '#4B5EFC';
-    btnRight[i].style.color = 'white';
+
+lightThemeBtn.onclick = () => {
+
+    console.log(body);
+
+    for (let i = 0; i < number.length; i++) {
+        number[i].style.backgroundColor = 'white';
+        number[i].style.color = 'black';
+    }
+
+    for (let j = 0; j < btnTop.length; j++) {
+        btnTop[j].style.backgroundColor = '#4E505F';
+        console.log(btnTop[j]);
+    }
+
+    for (let k = 0; k < btnRight.length; k++) {
+        btnRight[k].style.backgroundColor = '#4B5EFC';
+        btnRight[k].style.color = 'white';
+    }
+
+    body.style.backgroundColor = '#D2D3DA';
+    calculator.style.backgroundColor = '#D2D3DA';
+
+    btnTop.style.backgroundColor = '#D2D3DA';
+    btnRight.style.backgroundColor = '#4B5EFC';
+    btnRight.style.color = 'white';
+
+    deleteIcon.style.src = '../img/delete-Icon.svg';
+    userInput.style.color = 'black';
+    userInput.style.background = '#D2D3DA';
+
+    result.style.color = 'black';
+    result.style.background = '#D2D3DA';
+    console.log(result.style);
+    console.log(userInput.style);
 }
+
 
 for (let i = 0; i < number.length; i++) {
     // number[i].style.backgroundColor = 'white';
     // number[i].style.color = 'black';
 
-    lightThemeBtn.onclick = () => {
 
-        number[i].style.backgroundColor = 'white';
-        number[i].style.color = 'black';
-
-        for (let i = 0; i < btnTop.length; i++) {
-            btnTop[i].style.backgroundColor = '#D2D3DA';
-        }
-        calculator.backgroundColor = '#F1F2F3';
-
-        btnTop.style.backgroundColor = '#D2D3DA';
-        btnRight.style.backgroundColor = '#4B5EFC';
-        btnRight.style.color = 'white';
-
-        deleteIcon.style.src = '../img/delete-Icon.svg';
-    }
 
     number[i].onclick = () => {
 
